73d9409586ce28bf0fbfcb718fd01264dfae6698
İbrahim Gedik <67916168+ibrahimgediktr@users.noreply.github.com>
2021-04-02 00:26:37 +0300

    created features section animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     var toggleMenu = $('.toggle-menu');
     var navList = $('.navbar-list');
     var navLinks = $('.navbar-link');
@@ -8,21 +9,21 @@ $(document).ready(function () {
         navList.toggleClass('active');
         toggleMenu.toggleClass('active');
         gsap.fromTo(navLinks, {
-            delay:0.2,
-            x:100,
-            scale:0,
+            delay: 0.2,
+            x: 100,
+            scale: 0,
         }, {
-            x:0,
-            scale:1,
-            stagger:0.2
+            x: 0,
+            scale: 1,
+            stagger: 0.2
         })
     });
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 20) {
-        nav.addClass("scrolled");
+            nav.addClass("scrolled");
         } else {
-        nav.removeClass("scrolled");
+            nav.removeClass("scrolled");
         }
     });
 
@@ -47,7 +48,7 @@ $(document).ready(function () {
     gsap.from('.after-bg-path-1', {
         delay: 0.4,
         opacity: 0,
-        scale:0,
+        scale: 0,
     }, "=.2")
     gsap.from('.after-bg-path-2', {
         delay: 0.6,
@@ -76,32 +77,173 @@ $(document).ready(function () {
 
     // Hero Before Background Animations
     gsap.from('.before-bg-path-1', {
-        delay:0.8,
-        scaleX:0,
-        opacity:0,
+        delay: 0.8,
+        scaleX: 0,
+        opacity: 0,
     })
     gsap.from('.before-bg-path-2', {
-        delay:0.6,
-        opactiy:0,
-        scale:0,
+        delay: 0.6,
+        opacity: 0,
+        scale: 0,
     })
     gsap.from('.before-bg-path-3', {
-        delay:1,
-        opactiy:0,
-        scaleY:0,
+        delay: 1,
+        opacity: 0,
+        scaleY: 0,
     })
 
-    // Hero Container 
+    // Hero Section Animation
     gsap.from('.hero-heading', {
-        delay:1,
-        opacity:0,
-        y:40,
+        delay: 1,
+        opacity: 0,
+        y: 40,
     })
     gsap.from('.hero-description', {
-        delay:1.2,
-        opacity:0,
-        y:40,
+        delay: 1.2,
+        opacity: 0,
+        y: 40,
     })
+
+    // Features Section Animation
+    gsap.registerPlugin(ScrollTrigger);
+
+
+    gsap.set('.features-heading', {
+        x: -1000,
+        opacity: 0
+    })
+
+    ScrollTrigger.create({
+        trigger: '.features-heading',
+        start: 'top 90%',
+        end: 'top 10%',
+        onEnter: () => gsap.to('.features-heading', {
+            x: 0,
+            opacity: 1,
+        }),
+        onLeaveBack: () => gsap.to('.features-heading', {
+            x: -1000,
+            opacity: 0,
+        })
+    })
+
+    gsap.utils.toArray('.features-list').forEach(() => {
+        var elems = $('.features-list-item');
+
+        gsap.set(elems, {
+            x: 1000,
+            opacity: 0,
+        })
+        ScrollTrigger.create({
+            trigger: '.features-list-item',
+            start: 'top 80%',
+            end: 'top 20%',
+            markers: true,
+            onEnter: () => gsap.to(elems, {
+                x: 0,
+                opacity: 1,
+                stagger: 0.2,
+                delay: 0.2,
+            }),
+            onLeaveBack: () => gsap.to(elems, {
+                x: 1000,
+                opacity: 0,
+                duration: 0.5,
+                stagger: 0.2,
+                delay: 0.2,
+            })
+        })
+    });
+
+    gsap.set('.features-after-bg', {
+        x: 1000,
+        opacity: 0
+    });
+    gsap.set('.features-after-bg-path-1', {
+        scaleY: 0,
+        opacity: 0
+    });
+    gsap.set('.features-after-bg-path-2', {
+        scale: 0,
+        opacity: 0
+    });
+    gsap.set('.features-after-bg-path-3', {
+        scaleY: 0,
+        opacity: 0
+    });
+
+    ScrollTrigger.create({
+        trigger: '.features-after-bg',
+        start: 'top 90%',
+        end: 'top 10%',
+        onEnter: () => gsap.to('.features-after-bg', {
+            x: 0,
+            opacity: 1,
+            delay: 0.4,
+        }),
+        onLeaveBack: () => gsap.to('.features-after-bg', {
+            x: 1000,
+            opacity: 0,
+            delay: 0.4,
+        })
+    })
+    ScrollTrigger.create({
+        trigger: '.features-after-bg-path-1',
+        start: 'top 90%',
+        end: 'top 10%',
+        onEnter: () => gsap.to('.features-after-bg-path-1', {
+            scaleY: 1,
+            opacity: 1,
+            delay: 0.6,
+        }),
+        onLeaveBack: () => gsap.to('.features-after-bg-path-1', {
+            scaleY: 0,
+            opacity: 0,
+            delay: 0.6,
+        })
+    })
+
+    ScrollTrigger.create({
+        trigger: '.features-after-bg-path-1',
+        start: 'top 90%',
+        end: 'top 10%',
+        onEnter: () => gsap.to('.features-after-bg-path-2', {
+            scale: 1,
+            opacity: 1,
+            delay: 0.8,
+        }),
+        onLeaveBack: () => gsap.to('.features-after-bg-path-2', {
+            scale: 0,
+            opacity: 0,
+            delay: 0.8,
+        })
+    })
+
+    ScrollTrigger.create({
+        trigger: '.features-after-bg-path-3',
+        start: 'top 90%',
+        end: 'top 10%',
+        onEnter: () => gsap.to('.features-after-bg-path-3', {
+            scale: 1,
+            opacity: 1,
+            delay: 1,
+        }),
+        onLeaveBack: () => gsap.to('.features-after-bg-path-3', {
+            scaleY: 0,
+            opacity: 0,
+            delay: 1,
+        })
+    })
+
+
+
+
+
+
+
+
+
+
 })
 
 console.log('javascript is running');
