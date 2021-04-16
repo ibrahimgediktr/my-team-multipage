96a816fca4bfa1e8b9fd361d1a4186c4c8adfc86
İbrahim Gedik <67916168+ibrahimgediktr@users.noreply.github.com>
2021-04-16 10:10:54 +0300

    created about page animations

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,3 +1,62 @@
+// About Hero Section Animations
+gsap.from('.about-hero-heading', {
+    delay: 1,
+    opacity: 0,
+    y: 40,
+})
+gsap.from('.about-hero-description', {
+    delay: 1.2,
+    opacity: 0,
+    y: 40,
+})
+
+
+// About Hero After Background Animation
+gsap.from('.about-hero-after-bg-path-1', {
+    delay: 0.2,
+    opacity: 0,
+    scale: 0,
+}, "=.2")
+
+gsap.from('.about-hero-after-bg-path-2', {
+    delay: 0.6,
+    opacity: 0,
+    scale: 0,
+}, "=.2")
+
+gsap.from('.about-hero-after-bg-path-3', {
+    delay: 1,
+    opacity: 0,
+    scaleX: 0,
+}, "=.2")
+
+// Directors Section Before Background Animation
+gsap.from('.directors-before-bg-path-1', {
+    delay: 0.2,
+    opacity: 0,
+    scale: 0,
+}, "=.2")
+
+gsap.from('.directors-before-bg-path-2', {
+    delay: 0.4,
+    opacity: 0,
+    scale: 0,
+}, "=.2")
+
+gsap.from('.directors-before-bg-path-3', {
+    delay: 0.8,
+    opacity: 0,
+    scale: 0,
+}, "=.2")
+
+gsap.from('.directors-before-bg-path-4', {
+    delay: 1.4,
+    opacity: 0,
+    scaleY: 0,
+}, "=.2")
+
+
+
 // About CTA Section Animation
 ScrollTrigger.create({
     trigger: '.cta-after-bg',
@@ -6,7 +65,7 @@ ScrollTrigger.create({
     end: 'top 0%',
 })
 
-// Meet Directors Hidden Menu
+// Meet Directors Hidden Card
 var directorsCardBtn = $('.directors-card-btn');
 
 directorsCardBtn.each(function () {
